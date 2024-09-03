84535b0b87b440cd5108d434e5718604817893b9
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2024-09-03 14:46:50 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const player = {
+const playerBoard = {
     "board": document.getElementsByClassName("playerSide"),
     "column1": [document.getElementsByClassName("1 player"),document.getElementsByClassName("4 player"),document.getElementsByClassName("7 player")]
 }
