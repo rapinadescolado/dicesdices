e90cdd03d01611ea7d31541e3b9004fb686de7bc
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2024-09-03 15:00:03 -0300

    .

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const playerBoard = {
     "board": document.getElementsByClassName("playerSide"),
     "column1": [document.getElementsByClassName("1 player"),document.getElementsByClassName("4 player"),document.getElementsByClassName("7 player")]
 }
+
+setInterval(() => {
+    playerBoard.board.style.margin = "20px"
+}, 1000);
