544dee6eded65338f7d12d50beca50d733c6c8ae
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2024-09-03 17:43:47 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const playerBoard = {
     "board": document.getElementsByClassName("playerSide"),
-    "column1": [document.getElementsByClassName("1 player"),document.getElementsByClassName("4 player"),document.getElementsByClassName("7 player")]
+    "cima": [document.getElementsByClassName("1 player"),document.getElementsByClassName("2 player"),document.getElementsByClassName("3 player")],
+    "meio": [document.getElementsByClassName("4 player"),document.getElementsByClassName("5 player"),document.getElementsByClassName("6 player")],
+    "baixo": [document.getElementsByClassName("7 player"),document.getElementsByClassName("8 player"),document.getElementsByClassName("9 player")]
 }
+
+playerBoard.style.backgroundColor = "#fff";
