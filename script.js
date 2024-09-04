149800c9082a61367dda227ed4bae06d8cf9d6b2
Rapina <rapina.sv04@gmail.com>
2024-09-04 14:38:12 -0300

    Criação do menu e function Start

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// Definir as variaveis de acordo com o modo de jogo
+var gamemode;
+function start(modo) {
+    if(modo == 'pvp'  && gamemode == undefined) {
+        gamemode = modo;
+        document.getElementById("menu").style.animationPlayState = "running";
+    }
+    if (modo == 'bot'  && gamemode == undefined) {
+        // fazer mais tarde
+    }
+}
+
+// Variaveis dos tabuleiros e function pra retornar slots
 const leftSide = document.getElementById("leftSide");
 const rightSide = document.getElementById("leftSide");
+const leftPlayer = document.getElementById("leftPlayer");
+const rightPlayer = document.getElementById("rightPlayer");
 function getSlot(board,number) {
     let id = `${board}Side`;
     let slot = number - 1;
     return document.getElementById(id).getElementsByClassName("slot")[slot];
 }
 
-function start() {
+// Iniciar jogo. Modo: Dois jogadores
+if (gamemode == "pvp") {
     
 }
