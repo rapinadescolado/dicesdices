c0ba9c6df69e9cba65e21c1c575b665b0b509977
Rapina <rapina.sv04@gmail.com>
2024-09-03 23:25:54 -0300

    Arrumando código antigo

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const playerBoard = {
-    "board": document.getElementsByClassName("playerSide"),
-    "cima": [document.getElementsByClassName("1 player"),document.getElementsByClassName("2 player"),document.getElementsByClassName("3 player")],
-    "meio": [document.getElementsByClassName("4 player"),document.getElementsByClassName("5 player"),document.getElementsByClassName("6 player")],
-    "baixo": [document.getElementsByClassName("7 player"),document.getElementsByClassName("8 player"),document.getElementsByClassName("9 player")]
+const leftSide = document.getElementById("leftSide");
+const rightSide = document.getElementById("leftSide");
+function getSlot(board,number) {
+    let id = `${board}Side`;
+    let slot = number - 1;
+    return document.getElementById(id).getElementsByClassName("slot")[slot];
 }
 
-const rollButton = document.getElementById("rollButton")
-
-function roll () {
-    let dado = Math.floor(Math.random()*6 + 1);
-    rollButton.innerHTML = dado;
-    playerBoard.cima[1].src = "./img/dado${dado}.png"
+function start() {
+    
 }
