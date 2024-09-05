ffd5fcd4b0b9dd09b205ddef8a12a5462e03d12e
Rapina <rapina.sv04@gmail.com>
2024-09-04 22:33:42 -0300

    Mudança na tabela e girar dado

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function start(modo) {
     
     if(modo == 'pvp'  && gamemode == undefined) {
         gamemode = modo;
-
+        turn = "left";
         // MH: busque outra forma de animar a tela! Sem se apoiar no css, use apenas o javascript para animar a tela!
             // RV: Eu acho q é assim, ta certo?
-            
+
         document.getElementById("menu").style.top = "-100vh";
     }
     if (modo == 'bot'  && gamemode == undefined) {
@@ -26,13 +26,51 @@ function start(modo) {
 }
 
 
+
+
+
 // Variaveis dos tabuleiros e function pra retornar slots
-const leftSide = document.getElementById("leftSide");
-const rightSide = document.getElementById("leftSide");
-const leftPlayer = document.getElementById("leftPlayer");
-const rightPlayer = document.getElementById("rightPlayer");
-function getSlot(board,number) {
-    let id = `${board}Side`;
+function getSlot(side,number) {
+    let id = `${side}Side`;
     let slot = number - 1;
     return document.getElementById(id).getElementsByClassName("slot")[slot];
+}
+
+const leftSide = document.getElementById("leftSide");
+const leftPlayer = document.getElementById("leftPlayer");
+var leftSideMap = {
+    "cima": [getSlot("left", 1), getSlot("left", 2), getSlot("left", 3)],
+    "meio": [getSlot("left", 4), getSlot("left", 5), getSlot("left", 6)],
+    "baixo": [getSlot("left", 7), getSlot("left", 8), getSlot("left", 9)]
+}
+
+const rightSide = document.getElementById("rightSide");
+const rightPlayer = document.getElementById("rightPlayer");
+var rightSideMap = {
+    "cima": [getSlot("right", 1), getSlot("right", 2), getSlot("right", 3)],
+    "meio": [getSlot("right", 4), getSlot("right", 5), getSlot("right", 6)],
+    "baixo": [getSlot("right", 7), getSlot("right", 8), getSlot("right", 9)]
+}
+
+
+
+
+
+// Functions do PVP
+function roll() {
+    if (moveawait = "roll") {
+
+        let dado = Math.floor(Math.random()*6 + 1);
+        let button = document.getElementById("rollButton");
+        let result = document.getElementById("result");
+        button.style.transform = "rotate(720deg)";
+        button.innerHTML = ""
+        setTimeout(()=>{
+            button.style.display = "none";
+            result.style.display = "block";
+            result.src = `img/dado${dado}.png`;
+            moveawait = "select"
+        }, 2000)
+
+    }
 }
