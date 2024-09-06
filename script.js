4ba48d39563d327298b5924c40de450df3e3ff26
Rapina <rapina.sv04@gmail.com>
2024-09-05 23:08:56 -0300

    Novo sistema de colocar dados, e ajustes

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 var gamemode; // Serve para definir qual vai ser o codigo de jogo usado
 var turn; // Define quem está jogando, os valores podem ser "left" ou "right"
 var moveawait; // Define o movimento q ta esperando q o jogador no turn atual faça. // "roll" para jogar dado. "select" para selecionar slot
+var dadoAtual; // Dado que tirou no teste
+
 const leftSide = document.getElementById("leftSide");
 const leftPlayer = document.getElementById("leftPlayer");
 const rightSide = document.getElementById("rightSide");
 const rightPlayer = document.getElementById("rightPlayer");
-var leftPointsTotal = 0;    // Pontos totais (soma dos pontos de cada linha)
-var leftPointsMap = [0,0,0,0,0,0,0,0,0];  // Pontos de cada slot separado certinho
+
+var leftPointsTotal = 0;    // PointsTotal = Pontos totais(soma dos pontos de cada linha) // PointsMap = Pontos de cada slot separado certinho
+var leftPointsMap = [
+    {"slots":[0,0,0]},{"slots":[0,0,0]},{"slots":[0,0,0]}
+];
 var rightPointsTotal = 0;
-var rightPointsMap = [0,0,0,0,0,0,0,0,0];
-var dadoAtual; // Dado que tirou no teste
+var rightPointsMap = [
+    {"slots":[0,0,0]},{"slots":[0,0,0]},{"slots":[0,0,0]}
+];
 
 
 
@@ -22,6 +28,7 @@ function start(modo) {
     if(modo == 'pvp'  && gamemode == undefined) {
         gamemode = modo;
         turn = "left";
+        moveawait = "roll"
         menu.style.top = "-100vh";
         setTimeout(() => {
             menu.style.display = "none";
@@ -37,23 +44,19 @@ function start(modo) {
 
 
 // Function pra retornar slots e function para calcular pontos
-function getSlot(side,number) {
-    let id = `${side}Side`;
-    let slot = number - 1;
+function getSlot(side, line, number) {
+    side = `${side}Side`;
+    line = line - 1;
+    number = number - 1;
     let response = {
-        "button": document.getElementById(id).getElementsByClassName("slot")[slot],
-        "img": document.getElementById(id).getElementsByClassName("slot")[slot].firstChild
+        "line": document.getElementById(side).getElementsByClassName("line")[line],
+        "slot": document.getElementById(side).getElementsByClassName("line")[line].children[number],
+        "img": document.getElementById(side).getElementsByClassName("line")[line].children[number].firstChild
     };
     return response;
 }
 function calcPoints(side) {
-    if (side = "left") {
-        let cima = leftPointsMap[0] + leftPointsMap[1] + leftPointsMap[2];
-        let meio = leftPointsMap[3] + leftPointsMap[4] + leftPointsMap[5];
-        let baixo = leftPointsMap[6] + leftPointsMap[7] + leftPointsMap[8];
-
-        leftPointsTotal = cima + meio + baixo;
-    }
+    // vo fazer dps
 }
 
 
@@ -61,7 +64,7 @@ function calcPoints(side) {
 
 // Functions do PVP
 function roll() {
-    if (moveawait = "roll") {
+    if (moveawait == "roll") {
 
         dadoAtual = Math.floor(Math.random()*6 + 1);
         let button = document.getElementById("rollButton");
@@ -77,33 +80,50 @@ function roll() {
 
     }
 }
-function selectSlot(side, number) {
-    if (moveawait == "select" && side == turn) {
+function selectLine(line) {
+    if (moveawait == "select") {
 
-        // Calcular pontos e colocar imagem de dado no slot
-        if (side == "left") {
-            leftPointsMap[(number - 1)] = dadoAtual;
-        }else{
-            rightPointsMap[(number - 1)] = dadoAtual;
+        // Definir qual lado ta sendo usado e a line escolhida
+        let lineNumber = line;
+        let isFree = false;
+        if (turn == "left") {
+            pointMap = leftPointsMap[line].slots;
+            line = leftSide.children[line];
+        } else {
+            pointMap = rightPointsMap[line].slots;
+            line = rightSide.children[line];
         }
-        calcPoints(side)
-        getSlot(side, number).img.src = `img/dado${dadoAtual}.png`;
 
-        // Resetar o botão de dado
-        let button = document.getElementById("rollButton");
-        let result = document.getElementById("result");
-        button.style.display = "block";
-        button.style.transform = "rotate(0)";
-        button.innerHTML = "ROLAR DADO";
-        result.style.display = "none";
-
-        // Passar turno
-        moveawait = "roll";
-        if (side == "left") {
-            turn = "right";
-        }else{
-            turn = "left";
+        // Checar se tem espaço vazio na line e colocar dado na linha
+        if (pointMap[2] == 0) {
+            // Tem espaço no primeiro slot
+            getSlot(turn, lineNumber + 1, 3).img.src = `img/dado${dadoAtual}.png`;
+            pointMap[2] = dadoAtual;
+            isFree = true;
+        } else if (pointMap[1] == 0) {
+            // Tem espaço no segundo slot
+            getSlot(turn, lineNumber + 1, 2).img.src = `img/dado${dadoAtual}.png`;
+            pointMap[1] = dadoAtual;
+            isFree = true;
+        } else if (pointMap[0] == 0) {
+            // Tem espaço no terceiro slot
+            getSlot(turn, lineNumber + 1, 1).img.src = `img/dado${dadoAtual}.png`;
+            pointMap[0] = dadoAtual;
+            isFree = true;
+        } else {
+            // Não tem espaço
+            getSlot(turn, lineNumber + 1, 1).line.style.outline = "0.1vw red solid";
+            setTimeout(() => {
+                getSlot(turn, lineNumber + 1, 1).line.style.outline = "";
+            }, 150);
         }
-        console.log(leftPointsTotal + "and" + rightPointsTotal)
+
+        // Resetar botão de rolar e passar turno caso o dado tiver sido colocado
+        if (isFree == true) {
+            // to fazendo
+        }
+
+        // Atualizar variaveis
+        leftPointsMap[lineNumber].slots = pointMap;
     }
 }
